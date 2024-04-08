be5cc454d07d38db5fb8cd05c23901d76649b3ac
Caleb Lugo <cnl1211999@gmail.com>
2024-04-08 16:30:06 -0700

    feat: implement homepage button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import {createHomepage} from "./homepage.js";
 
+const homepageBtn = document.querySelector('#home');
+const menuBtn = document.querySelector('#menu');
+const aboutBtn = document.querySelector('#about');
+
 createHomepage();
+
+homepageBtn.addEventListener('click', () => {
+    clear();
+    createHomepage();
+});
+
+function clear() {
+    document.querySelector('#content').innerHTML = '';
+}
