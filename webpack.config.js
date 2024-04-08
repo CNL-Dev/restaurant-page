42b2f9bccb6de9e6fde0648a21a29199fb574c42
Caleb Lugo <cnl1211999@gmail.com>
2024-04-08 16:02:26 -0700

    chore: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,25 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
   mode: 'development'
 };
