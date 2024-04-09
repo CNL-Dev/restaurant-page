7d87904f0dd9ccf2487c827accd3b7bd05756996
Caleb Lugo <cnl1211999@gmail.com>
2024-04-09 15:49:15 -0700

    feat: implement menu button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {createHomepage} from "./homepage.js";
+import { createMenuPage } from "./menupage.js";
 
 const homepageBtn = document.querySelector('#home');
 const menuBtn = document.querySelector('#menu');
@@ -9,6 +10,11 @@ createHomepage();
 homepageBtn.addEventListener('click', () => {
     clear();
     createHomepage();
+});
+
+menuBtn.addEventListener('click', () => {
+    clear();
+    createMenuPage();
 });
 
 function clear() {
